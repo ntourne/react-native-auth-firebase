3cdeaef839323c2eed4bdb012a3cbb51b6c3fd78
Nicolas Tourne <ntourne@beerealit.com>
2017-08-12 12:19:04 -0300

    Add common components

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Header, Button } from './components/common';
 
 class App extends Component {
     render() {
         return (
             <View>
+                <Header headerText="Authentication"></Header>
                 <Text>An App!</Text>
+                <Button>A button</Button>
             </View>
         )
     }
