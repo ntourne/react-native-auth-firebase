5c8d1e32bc728dcbcb7d4aaae9d5a06e952a7635
Nicolas Tourne <ntourne@beerealit.com>
2017-08-12 12:49:33 -0300

    Add text input

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
 import { Button, Card, CardSection } from './common';
+import { TextInput } from 'react-native';
 
 class LoginForm extends Component {
+
+    state = { text: 'text' }
 
     render() {
         return (
             <Card>
-                <CardSection />
+                <CardSection>
+                    <TextInput 
+                        value={this.state.text}
+                        onChangeText={text => this.setState({ text })}
+                        style={{ height: 20, width: 100 }}
+                    />
+                </CardSection>
                 <CardSection />
                 <CardSection>
                     <Button>Log in</Button>
