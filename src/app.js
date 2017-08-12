69758ab15c064cae73257dd5041152d924dc2c6b
Nicolas Tourne <ntourne@beerealit.com>
2017-08-12 12:35:42 -0300

    Create LoginForm

--- src/app.js
+++ src/app.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header, Button } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+
+
+    componentWillMount() {
+
+        // Initialize Firebase
+        firebase.initializeApp({
+            apiKey: "AIzaSyBhpzZrZ0JA7LfRY9ZMoJK8b1uCqp43XRY",
+            authDomain: "react-native-auth-850d2.firebaseapp.com",
+            databaseURL: "https://react-native-auth-850d2.firebaseio.com",
+            projectId: "react-native-auth-850d2",
+            storageBucket: "react-native-auth-850d2.appspot.com",
+            messagingSenderId: "27901225345"
+        });
+    }
+
+    
     render() {
         return (
             <View>
                 <Header headerText="Authentication"></Header>
-                <Text>An App!</Text>
-                <Button>A button</Button>
+                <LoginForm />
             </View>
         )
     }
